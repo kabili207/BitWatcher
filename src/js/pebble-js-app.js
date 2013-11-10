1a85894e239051b42a056c11e0b45e9cb44db5c2
Andrew Nagle <kabili@zyrenth.com>
2013-11-10 13:54:55 -0500

    Added configuration screen. For some reason the pebble app seems to crash when trying to use the dropdown options though. Could be a bug in the SDK.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,21 +1,51 @@
+var initialized = false;
+
+function getOptions() {
+	var options = JSON.parse(window.localStorage.getItem('options'));
+	
+	if(options == null) {
+		options = { currency: "USD", exchange: "", average: true, noGox: true };
+		window.localStorage.setItem('options', JSON.stringify(options));
+	}
+	return options;
+}
 
 function getPrice() {
 	var response;
 	var req = new XMLHttpRequest();
-	console.log("Sending...");
-	// Token doesn't seem to be unique, as the API would suggest.
-	// Consider migrating to direct call to BitcoinAverage API,
-	// utilizing the config screen.
-	console.log("Token: " + Pebble.getAccountToken());
-	req.open('GET', "http://pebble.zyrenth.com/btc_price.json", true);
-	req.setRequestHeader("X-Pebble-ID", Pebble.getAccountToken());
+	
+	var options = getOptions();
+	
+	var url;
+	var rates;
+	
+	if(options.average) {
+		if(options.noGox)
+			url = "https://api.bitcoinaverage.com/no-mtgox/ticker/" + options.currency;
+		else
+			url = "https://api.bitcoinaverage.com/ticker/" + options.currency;
+	} else {
+		url = "https://api.bitcoinaverage.com/exchanges/" + options.currency;
+	}
+	
+	req.open('GET', url, true);
 	req.onreadystatechange = function(e) {
 		if (req.readyState == 4) {
 			if(req.status == 200) {
-				console.log(req.responseText);
-				response = JSON.parse(req.responseText);
+				rates = response = JSON.parse(req.responseText);
+				if(!options.average)
+					rates = response[options.exchange].rates;
+				var message = {
+					"currency": options.currency,
+					"exchange" : options.average ? (options.noGox ? " (no mtgox)" : "") : options.exchange,
+					"ask": rates.ask.toString(),
+					"bid": rates.bid.toString(),
+					"last": rates.last.toString()
+				};
 				
-				Pebble.sendAppMessage(response);
+				console.log("Sending...");
+				console.log(JSON.stringify(message));
+				Pebble.sendAppMessage(message);
 				
 			} else {
 				console.log("Error " + req.status);
@@ -28,6 +58,7 @@ function getPrice() {
 Pebble.addEventListener("ready", function(e) {
 	console.log("connect!" + e.ready);
 	console.log(e.type);
+	initialized = true;
 	getPrice();
 });
 
@@ -40,8 +71,23 @@ Pebble.addEventListener("appmessage", function(e) {
 
 });
 
-Pebble.addEventListener("webviewclosed", function(e) {
-	console.log("webview closed");
-	console.log(e.type);
-	console.log(e.response);
+Pebble.addEventListener("showConfiguration", function() {
+	var options = getOptions();
+	console.log("read options: " + JSON.stringify(options));
+	console.log("showing configuration");
+	var uri = 'https://raw.github.com/kabili207/BitWatcher/master/config.html?config=' + encodeURIComponent(JSON.stringify(options));
+	//var uri = 'http://zyrenth.com/config.html?config=' + encodeURIComponent(JSON.stringify(options));
+	Pebble.openURL(uri);
+});
+
+Pebble.addEventListener("configurationClosed", function(e) {
+	console.log("configuration closed");
+	if (e.response != '') {
+		var options = JSON.parse(e.configurationData);
+		console.log("storing options: " + JSON.stringify(options));
+		window.localStorage.setItem('options', JSON.stringify(options));
+		getPrice();
+	} else {
+		console.log("no options received");
+	}
 });
